0341789d875c58163fd01d320d5d1a8cd0d70a81
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2020-11-23 15:59:47 -0500

    working dnd with overlap and reset

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import Tile from './components/Tile.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.tileContainer}>
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+      </View>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+  tileContainer: {
+    // flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    // backgroundColor: '#68BEF6',
+    // height: '80%'
+  }
+})
---
+++ components/Tile.js
@@ -0,0 +1,82 @@
+import React, { useState, useRef, Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  PanResponder,
+  Animated
+} from 'react-native'
+
+import Draggable from 'react-native-draggable'
+
+class Tile extends Component {
+  constructor(props) {
+    super(props)
+    const position = new Animated.ValueXY()
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        this.setState({
+          zIndex: 999,
+        })
+        position.setValue({ x: gesture.dx, y: gesture.dy })
+      },
+      onPanResponderRelease: (evt, gesture) => {
+        this.setState({
+          zIndex: 0
+        })
+        position.setValue({ x: 0, y: 0 })
+      },
+    })
+
+    this.state = { panResponder, position, zIndex: 0 }
+  }
+
+  render() {
+    let handles = this.state.panResponder.panHandlers;
+
+    return (
+      <View style={{...styles.empty, zIndex: this.state.zIndex}}>
+      <Animated.View
+        style={[this.state.position.getLayout(), {zIndex: this.state.zIndex, position: 'absolute'}]}  
+        {...handles}
+      >
+        <View style={styles.container}>
+          <Text style={styles.letter}>A</Text>
+
+          <View style={styles.box} />
+        </View>
+      </Animated.View>
+      </View>
+    )
+  }
+}
+
+export default Tile
+
+const styles = StyleSheet.create({
+  empty:{
+    height: 60,
+    width: 60,
+    margin: 5,
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    height: 60,
+    width: 60,
+    margin: 5,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'black',
+    position: 'absolute'
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  letter: {
+    fontSize: 30
+  }
+})
